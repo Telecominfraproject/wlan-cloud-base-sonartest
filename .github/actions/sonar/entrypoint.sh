4f3fb1138715b907ebefd7732aba2962ce55f615
Eugene Taranov <eugene@opsfleet.com>
2020-06-16 02:19:24 +0300

    15

--- .github/actions/sonar/entrypoint.sh
+++ .github/actions/sonar/entrypoint.sh
@@ -1,3 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh -e
 
-mvn clean verify sonar:sonar -Dsonar.host.url=${{ env.SONAR_URL }} -Dsonar.login=${{ secrets.SONAR_LOGIN }} -Dsonar.organization=${{ env.SONAR_ORGANIZATION }} -Dsonar.projectKey=${{ env.SONAR_PROJECT_KEY }}
+cd $INPUT_PROJECT_DIR
+mvn clean verify sonar:sonar $INPUT_SONAR_ARGS
